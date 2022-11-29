c9b2b625c7745f33a3c01d82da0a7bd85f752491
AshtonBurgess <w0465511@nscc.ca>
2022-11-29 18:53:42 -0400

    phase 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('posts.index') }}">{{ __('Posts') }}</a>
+                        </li>
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -49,6 +52,18 @@
                                 </li>
                             @endif
                         @else
+
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('posts.create') }}">{{ __('Create Posts') }}</a>
+                            </li>
+
+
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('posts.index') }}">{{ __('Posts') }}</a>
+                            </li>
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
